e81ece11b69c483efe26e6d0b51b48f91ad8181e
vikaspachdha <vikaspachdha@gmail.com>
2014-01-05 00:17:58 +0530

    Country info

--- src/der_artikel/der_artikel_hin.ts
+++ src/der_artikel/der_artikel_hin.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
+<TS version="2.0" language="hi_IN" sourcelanguage="en_US">
 <context>
     <name>About_page</name>
     <message>
