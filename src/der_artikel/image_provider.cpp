ac72124c61c3d56c0f3e4fbce9a04d86a939842e
vikaspachdha <vikaspachdha@gmail.com>
2014-05-04 14:11:36 +0530

    Documentation.

--- src/der_artikel/image_provider.cpp
+++ src/der_artikel/image_provider.cpp
@@ -1,34 +1,74 @@
-#include "image_provider.h"
-#include "log4qt/log_defines.h"
-/*!
- \brief
+//******************************************************************************
+/*! \file image_provider.cpp Implementation of \ref ImageProvider_C
+ *
+ *  \author Vikas Pachdha
+ *
+ *  \copyright Copyright (C) 2014 Vikas Pachdha, Mohita Gandotra.
+ * Contact: http://www.vikaspachdha.com
+ *
+ * This file is part of the application der_artikel.
+ *
+ *  \copyright GNU Lesser General Public License Usage
+ * This file may be used under the terms of the GNU Lesser
+ * General Public License version 2.1 as published by the Free Software
+ * Foundation and appearing in the file LICENSE.LGPL included in the
+ * packaging of this file.  Please review the following information to
+ * ensure the GNU Lesser General Public License version 2.1 requirements
+ * will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+ *
+ *  \copyright GNU General Public License Usage
+ * Alternatively, this file may be used under the terms of the GNU
+ * General Public License version 3.0 as published by the Free Software
+ * Foundation and appearing in the file LICENSE.GPL included in the
+ * packaging of this file.  Please review the following information to
+ * ensure the GNU General Public License version 3.0 requirements will be
+ * met: http://www.gnu.org/copyleft/gpl.html.
+ *
+ ******************************************************************************/
 
-*/
+// Interface for this file
+#include "image_provider.h"
+
+// Framework and lib includes
+#include "log4qt/log_defines.h"
+
+
+//******************************************************************************
+/*! \brief Constructor
+ *
+ *  \author Vikas Pachdha
+ ******************************************************************************/
 ImageProvider_C::ImageProvider_C() :
     QQuickImageProvider(QQuickImageProvider::Pixmap)
 {
 }
 
-/*!
- \brief
-
- \param id
- \param pixmap
-*/
+//******************************************************************************
+/*! \brief Add image to the store against the id.
+ *
+ *  \author Vikas Pachdha
+ *
+ *  \param[in] id : Id of the image.
+ ******************************************************************************/
 void ImageProvider_C::AddImage(QString id, QPixmap pixmap)
 {
     LOG_INFO("Image provider :: Image added for id - "+id);
     _images[id] = pixmap;
 }
 
-/*!
- \brief
-
- \param id
- \param size
- \param requestedSize
- \return QPixmap
-*/
+//******************************************************************************
+/*! \brief Returns image against the requested id. For unavailable images, null
+ *  pixmaps shall be returned.
+ *
+ *  \author Vikas Pachdha
+ *
+ *  \param[in] id : Id of the image requested.
+ *  \param[in] size : Original size of the image.
+ *  \param[in] requestedSize : corresponds to the Image::sourceSize requested by
+ *  an Image item.
+ *
+ *  \return QPixmap : Requested image.
+ ******************************************************************************/
 QPixmap ImageProvider_C::requestPixmap(const QString &id, QSize *size, const QSize &requestedSize)
 {
     Q_UNUSED(size);
--- src/der_artikel/image_provider.h
+++ src/der_artikel/image_provider.h
@@ -1,8 +1,43 @@
+//******************************************************************************
+/*! \file image_provider.h Declaration of \ref ImageProvider_C
+ *
+ *  \author Vikas Pachdha
+ *
+ *  \copyright Copyright (C) 2014 Vikas Pachdha, Mohita Gandotra.
+ * Contact: http://www.vikaspachdha.com
+ *
+ * This file is part of the application der_artikel.
+ *
+ *  \copyright GNU Lesser General Public License Usage
+ * This file may be used under the terms of the GNU Lesser
+ * General Public License version 2.1 as published by the Free Software
+ * Foundation and appearing in the file LICENSE.LGPL included in the
+ * packaging of this file.  Please review the following information to
+ * ensure the GNU Lesser General Public License version 2.1 requirements
+ * will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+ *
+ *  \copyright GNU General Public License Usage
+ * Alternatively, this file may be used under the terms of the GNU
+ * General Public License version 3.0 as published by the Free Software
+ * Foundation and appearing in the file LICENSE.GPL included in the
+ * packaging of this file.  Please review the following information to
+ * ensure the GNU General Public License version 3.0 requirements will be
+ * met: http://www.gnu.org/copyleft/gpl.html.
+ *
+ ******************************************************************************/
 #ifndef IMAGE_PROVIDER_H
 #define IMAGE_PROVIDER_H
 
-#include <QQuickImageProvider>
+// System includes
 #include <QHash>
+#include <QQuickImageProvider>
+
+
+//******************************************************************************
+/*! \brief Main image provider for the QML.
+ *
+ *  \author Vikas Pachdha
+ ******************************************************************************/
 class ImageProvider_C : public QQuickImageProvider
 {
 public:
@@ -15,6 +50,7 @@ public:
     virtual QPixmap	requestPixmap(const QString & id, QSize * size, const QSize & requestedSize);
 
 private:
+    //! Stores images with their id's
     QHash<QString, QPixmap> _images;
 };
 
